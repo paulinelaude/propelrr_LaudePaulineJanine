97f8486d2ee1b16a2e6805b200d897a8c7a8add1
paulinelaude <paulinelaude@gmail.com>
2023-09-28 23:36:04 +0800

    second commit

--- application/views/fabonacci.php
+++ application/views/fabonacci.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Propelrr Exam - LaudePaulineJanine</title>
+    <title>Propelrr Exam - Fabonacci</title>
 
     <script src="<?php echo base_url(); ?>assets/js/jquery-2.1.1.min.js" type="text/javascript"></script>
     <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" media="screen" />
